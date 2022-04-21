c3f76b8110d745e7aadb63ecf2071e1f68a9bf24
juanp-rojash <juanp.rojash@upb.edu.co>
2022-04-20 23:44:53 -0500

    Actualizacion - Conexion a Base de  Datos

---
+++ Generador de Datos - IoT/Clases/Sensor.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace Generador_de_Datos___IoT.Clases
+{
+    public class Sensor
+    {
+        private string tipo;
+        private int dato;
+        private int sector;
+
+        public Sensor()
+        {
+            this.tipo = "";
+            this.dato = 0;
+            this.sector = 0;
+        }
+
+        public Sensor(string tipo, int dato, int sector)
+        {
+            this.tipo = tipo;
+            this.dato = dato;
+            this.sector = sector;
+        }
+
+        [BsonId]
+        public BsonObjectId Id { get; set; }
+
+        [BsonElement("tipo")]
+        public string Tipo { get => tipo; set => tipo = value; }
+
+        [BsonElement("dato")]
+        public int Dato { get => dato; set => dato = value; }
+
+        [BsonElement("sector")]
+        public int Sector { get => sector; set => sector = value; }
+    }
+}
